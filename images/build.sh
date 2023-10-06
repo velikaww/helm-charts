9f87b1fcac3376ae9ff558052fe57f33756cc8fa
Regis WANG <wei.wang@mathartsys.com>
2023-10-06 23:04:30 +0800

    changed to use k8tz as template for svn-ssh

---
+++ images/build.sh
@@ -0,0 +1,28 @@
+
+# PARAM_TARGET_ARCH: 指定目标cpu架构
+
+set -e
+
+odir=$(pwd)
+dir="$(cd "$(dirname "$0")";pwd)"
+file="${dir}/$(basename "$0")"
+while file_symbol="$(readlink "$file")"; do
+    file="$(cd "$(dirname "$file")";cd "$(dirname "$file_symbol")";pwd)/$(basename "$file_symbol")"
+    dir="$(dirname "$file")"
+done
+
+_d=$1
+shift
+cd $dir/$_d
+_cross_build=""
+if [ -n "$PARAM_TARGET_ARCH" ] && [ "$PARAM_TARGET_ARCH" != "$(arch || : 2>/dev/null)" ]; then
+  _cross_build=" --platform=linux/${PARAM_TARGET_ARCH}"
+fi
+_tags=""
+while [ -n "$1" ] && [ "$1" != "--" ]; do
+  _tags=" -t $1"
+  shift
+done
+shift || :
+docker${_cross_build:+ buildx} build${_cross_build}${_tags} "$@" .
+cd $odir
---
+++ images/svnssh/start.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Launching svn on \"$DIR_SVN\", repos: $@"
+for _r in "$@"; do
+  if [[  -d "$DIR_REPO/$_r" ]]; then
+    echo "svn repo \"$_r\" has installed."
+  else
+    echo "Creating repo \"$_r\"..."
+    mkdir -p $DIR_REPO && svnadmin create $DIR_REPO/$_r
+    echo "Created repo \"$_r\"."
+  fi
+done
+echo "Starting sshd..,"
+/usr/sbin/sshd -D 
+
